ffd3e81a7b03643ac87549740e27ab205bcb7ce0
hiciritci <halilcrtc@gmail.com>
2024-11-11 14:21:46 +0300

    firstPush

---
+++ 1.Fundamentals/src/HesapMakinasi/CalculateService.cs
@@ -0,0 +1,7 @@
+﻿namespace HesapMakinasi;
+public class CalculateService
+{
+    public int Add(int x, int y) { return x + y; }
+
+    public int Subtract(int x, int y) { return x - y; }
+}
---
+++ 1.Fundamentals/test/HesapMakinasi.Tests.UnitTest/CalculateServiceTest.cs
@@ -0,0 +1,19 @@
+using HesapMakinasi;
+
+namespace TestProject1;
+
+public class CalculateServiceTest
+{
+    [Fact]
+    public void Add_ShouldSumdTwoInteger_When_HaveTwoInteger()
+    {
+        CalculateService service = new();
+        var response = service.Add(1, 2);
+
+        Assert.Equal(response,3);
+        Assert.NotEqual(response,4);    
+        Assert.NotNull(response);    
+
+        
+    }
+}
