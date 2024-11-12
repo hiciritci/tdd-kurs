6639676c916970dfc6b9abb99f05bdeb94df20d8
hiciritci <halilcrtc@gmail.com>
2024-11-12 16:14:20 +0300

    hata var 1 test calismiyor.

---
+++ TestDrivenDevelopment/3.Projects/StokTakip/tests/StokTakip.Web.API.Tests.UnitTest/Services/ProductServiceFixture.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GenericRepository;
+using NSubstitute;
+using StokTakip.WebAPI.Repositories;
+using StokTakip.WebAPI.Services;
+
+namespace StokTakip.WebAPI.Tests.UnitTest;
+public sealed class ProductServiceFixture
+{
+    public IProductRepository productRepo = Substitute.For<IProductRepository>();
+    public IUnitOfWork uow = Substitute.For<IUnitOfWork>();
+    public IMapper mapper = Substitute.For<IMapper>();
+    public readonly ProductService productService;
+    public ProductServiceFixture()
+    {
+        productService = new(productRepo, uow, mapper);
+    }
+}
--- TestDrivenDevelopment/3.Projects/StokTakip/tests/StokTakip.Web.API.Tests.UnitTest/Services/ProductServiceTest.cs
+++ TestDrivenDevelopment/3.Projects/StokTakip/tests/StokTakip.Web.API.Tests.UnitTest/Services/ProductServiceTest.cs
@@ -1,67 +1,100 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
 using FluentAssertions;
-using GenericRepository;
+using FluentValidation;
 using NSubstitute;
 using StokTakip.WebAPI.Dtos;
-using StokTakip.WebAPI.Repositories;
+using StokTakip.WebAPI.Models;
 using StokTakip.WebAPI.Services;
-using ValidationException = FluentValidation.ValidationException;
 
-namespace StokTakip.Web.API.Tests.UnitTest.Services;
-public sealed class ProductServiceTest : IClassFixture<ProductService>
+namespace StokTakip.WebAPI.Tests.UnitTest.Services;
+public sealed class ProductServiceTest : IClassFixture<ProductServiceFixture>
 {
-    private readonly ProductService productService;
+    private readonly ProductServiceFixture _fixture;
+    private readonly ProductService _sut;
 
-    public ProductServiceTest(ProductService fix)
+    public ProductServiceTest(ProductServiceFixture fix)
     {
-        productService = fix;
+        _fixture = fix;
+        _sut = fix.productService;
     }
-
     [Fact]
-    public async Task Create_Should_Throw_ValidationException_If_Name_Less_Then_3_Characters()
+    public async Task Create_Should_Throw_ValidationExeption_When_Name_Less_Than_3_Characters()
     {
-        /// Arange 
-        var productRepository = Substitute.For<IProductRepository>();
-        var unitofWork = Substitute.For<IUnitOfWork>();
-        var mapper = Substitute.For<IMapper>();
-        ProductService productService = new(productRepository, unitofWork, mapper);
-        CreateProductDto request = new("HC", 1, 1);
 
-        //Faker<CreateProductDto> faker = new Faker<CreateProductDto>().RuleFor(p => p.Name, f => f.Commerce.ProductName()).RuleFor(p => p.Price, f => Convert.ToDecimal(f.Commerce.Price(1, 1000, 2)));
-
-        //  var request1 = faker.Generate();
-
-
-        /// Act
-
-        var action = async () => await productService.CreateAsync(request, default);
-        /// Assert
-
+        CreateProductDto request = new("dm", 1, 1);
+        //Act
+        var action = async () => await _sut.CreateAsync(request, default);
+        //Assert
         var exception = await action.Should().ThrowAsync<ValidationException>();
         exception.Which.Errors.Should().HaveCount(1);
         exception.Which.Errors.First().ErrorMessage.Should().Be("Ürün adı en az 3 karakter olmalıdır");
     }
     [Fact]
-    public async Task Create_Should_Throw_ValidationException_If_Stock_Is_Zero()
+    public async Task Create_Should_Throw_ValidationExeption_When_Stock_Is_Zero()
     {
-        var prodRepo = Substitute.For<IProductRepository>();
-        var unit = Substitute.For<IUnitOfWork>();
-        var map = Substitute.For<IMapper>();
 
-        ProductService productService = new(prodRepo, unit, map);
-        CreateProductDto res = new("adi", 0, 1);
-        var act = async () => await productService.CreateAsync(res, default);
+        CreateProductDto request = new("ürün adı", 0, 1);
 
+        //act
+        var action = async () => await _sut.CreateAsync(request,
+            default);
 
-        var exc = await act.Should().ThrowAsync<ValidationException>();
-        exc.Which.Errors.Should().HaveCount(1);
-        exc.Which.Errors.First().ErrorMessage.Should().Be("Stok adedi 0 dan büyük olmalıdır");
+        //assert
+        var exeprtion = await action.Should().ThrowAsync<ValidationException>();
+        exeprtion.Which.Errors.Should().HaveCount(1);
+        exeprtion.Which.Errors.First().ErrorMessage.Should().Be("Stok adedi 0 dan büyük olmalıdır");
+
+    }
+    [Fact]
+    public async Task Create_Should_Throw_ValidationExeption_When_Price_Is_Zero()
+    {
+        CreateProductDto request = new("ürün adı", 10, 0);
+        //act
+        var action = async () => await _sut.CreateAsync(request,
+            default);
+
+        //assert
+        var exeprtion = await action.Should().ThrowAsync<ValidationException>();
+        exeprtion.Which.Errors.Should().HaveCount(1);
+        exeprtion.Which.Errors.First().ErrorMessage.Should().Be("Price adedi 0 dan büyük olmalıdır");
+
     }
 
     [Fact]
-    public async Task Create_Should_Throw_ArgumentException_if_Name_Already_Exists()
+    public async Task Create_Should_Throw_ArgumentException_When_Name_Already_Exists()
+    {
+        //Arrange
+        CreateProductDto request = new("Bilgisayar", 1, 1);
+        _fixture.productRepo.AnyAsync(Arg.Any<Expression<Func<Product, bool>>>(), default).Returns(Task.FromResult(true));
+
+        //act
+        var action = async () => await _sut.CreateAsync(request, default);
+
+        //assert
+
+        var exeprtion = await action.Should().ThrowAsync<ArgumentException>();
+
+        exeprtion.Which.Message.Should().Be("Ürün adı daha önce kullanılmış");
+
+    }
+
+    [Fact]
+    public async Task Create_should_Convert_dto_obj_when_request_is_valid_and_name_is_unique()
     {
         CreateProductDto request = new("Bilgisayar", 1, 1);
 
+        Product expected = new()
+        {
+            Name = request.Name,
+            Price = request.Price,
+            Stock = request.Stock
+        };
+
+        _fixture.mapper.Map<Product>(Arg.Any<CreateProductDto>()).Returns(expected);
+
+        //act
+        await _sut.CreateAsync(request, default);
     }
+
+
 }
