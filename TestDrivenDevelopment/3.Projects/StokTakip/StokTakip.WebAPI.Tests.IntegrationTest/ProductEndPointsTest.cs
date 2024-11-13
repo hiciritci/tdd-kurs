35b67b2154ba2d2d374ca8f6aecbbc77d07582e9
hiciritci <halilcrtc@gmail.com>
2024-11-13 14:44:01 +0300

    .....

--- TestDrivenDevelopment/3.Projects/StokTakip/StokTakip.WebAPI.Tests.IntegrationTest/ProductEndPointsTest.cs
+++ TestDrivenDevelopment/3.Projects/StokTakip/StokTakip.WebAPI.Tests.IntegrationTest/ProductEndPointsTest.cs
@@ -1,4 +1,8 @@
-﻿using FluentAssertions;
+﻿using System.Net.Http.Json;
+using Bogus;
+using FluentAssertions;
+using StokTakip.WebAPI.Dtos;
+using StokTakip.WebAPI.Models;
 
 namespace StokTakip.WebAPI.Tests.IntegrationTest;
 public sealed class ProductEndPointsTest
@@ -16,5 +20,27 @@ public sealed class ProductEndPointsTest
         //Assert
         message.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
 
+    }
+
+    [Fact]
+    public async Task Create_Should_Return_StatusCode_200_If_Request_Is_Valid_And_Name_Is_Unique()
+    {
+        //Arrenge
+        Faker faker = new Faker(); //BOGUS Kütüphanesi
+        CreateProductDto request = new(
+            faker.Commerce.ProductName(),
+            faker.Random.Number(1, 20),
+            Convert.ToDecimal(faker.Commerce.Price(1, 15000)));
+        HttpClient httpClient = new()
+        {
+            BaseAddress = new Uri("https://localhost:58431")
+        };
+        //Act
+        var message = await httpClient.PostAsJsonAsync("/products", request, default);
+        //Assert
+        message.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        Product? product = await message.Content.ReadFromJsonAsync<Product>();
+        product.Should().NotBeNull();
+        product!.Name.Should().Be(request.Name);
     }
 }
