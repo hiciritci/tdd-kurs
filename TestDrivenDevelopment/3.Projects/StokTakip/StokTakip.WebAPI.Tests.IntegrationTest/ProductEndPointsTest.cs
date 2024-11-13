c2663a6cee6a4b2af0a7b9c9a18b0fa5688d3327
hiciritci <halilcrtc@gmail.com>
2024-11-13 14:20:14 +0300

    IntegrationTest.OK

---
+++ TestDrivenDevelopment/3.Projects/StokTakip/StokTakip.WebAPI.Tests.IntegrationTest/ProductEndPointsTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace StokTakip.WebAPI.Tests.IntegrationTest;
+public sealed class ProductEndPointsTest
+{
+    [Fact]
+    public async Task GetAll_Should_Return_StatusCode_200_When_There_Is_No_Error()
+    {
+        //Arrange
+        HttpClient httpClient = new HttpClient()
+        { BaseAddress = new Uri("https://localhost:58431") };
+
+        //Act
+        var message = await httpClient.GetAsync("/products");
+
+        //Assert
+        message.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+    }
+}
