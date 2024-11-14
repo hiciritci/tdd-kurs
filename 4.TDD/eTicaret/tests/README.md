0dd704422562c98b01884e2bbcafc6bec390d2bb
hiciritci <halilcrtc@gmail.com>
2024-11-14 10:40:56 +0300

    ilkders

---
+++ 4.TDD/eTicaret/tests/README.md
@@ -0,0 +1,6 @@
+# TDD eTicaret Proje İçeriği
+-[ ] Kullanıcı Yapımız olmalı
+    -[ ] FirstName alanı 3 karakterden fazla olmalı.
+    -[ ] Lastname alanı 3  karakterdern fazla olmalı.
+    -[ ] Email alanı geçerli bir email ile birlikte 3 karakterden fazla olmalı.
+    -[ ] Password alanı bir şartımız yok.
---
+++ 4.TDD/eTicaret/tests/eTicaret.Domain.Tests.UnitTests/UserModelTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+
+namespace eTicaret.Domain.Tests.UnitTests;
+public sealed class UserModelTests
+{
+    [Fact]
+    public void FirstName_Should_Throw_Exception_If_Less_Than_3_Characters()
+    {
+        //Arrange
+        User user = new();
+        //Act
+        var action = () => user.SetName("Ha");
+
+        //Assert
+        action.Should().Throw<Exception>();
+
+    }
+
+    [Fact]
+    public void FirstName_Should_Not_Throw_Exception_If_Greater_Than_2_Characters()
+    {
+        //Arrange
+        User user = new();
+        //Act
+        var action = () => user.SetName("Halil");
+
+        //Assert
+        action.Should().NotThrow<Exception>();
+
+    }
+}
+public sealed class User
+{
+    public void SetName(string val)
+    {
+        throw new Exception();
+    }
+}
