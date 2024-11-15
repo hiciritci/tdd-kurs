ce5e8c27086c0064e28bce717985266f19ffc625
hiciritci <halilcrtc@gmail.com>
2024-11-15 14:19:15 +0300

    ....

--- 4.TDD/15.11.2024/4.TDD/eTicaret/src/eTicaret.WebAPI/Program.cs
+++ 4.TDD/15.11.2024/4.TDD/eTicaret/src/eTicaret.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using eTicaret.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -7,5 +10,17 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.MapControllers();
+
+
+//migartion her proje ayaða kaltýðýndan kendi oluþtutur.Canlýda istenen bir durum deðil. if blogu bu sebeple konuldu.
+if (app.Environment.IsDevelopment())
+{
+    using (var scoped = app.Services.CreateScope())
+    {
+        var dbContex = scoped.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContex.Database.Migrate();
+    }
+}
+
 
 app.Run();
