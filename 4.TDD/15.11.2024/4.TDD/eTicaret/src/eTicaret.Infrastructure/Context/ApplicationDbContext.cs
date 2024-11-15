d8afa0596e505983a04e7d3f9366933c7eccb125
hiciritci <halilcrtc@gmail.com>
2024-11-15 12:01:57 +0300

    ...

---
+++ 4.TDD/15.11.2024/4.TDD/eTicaret/src/eTicaret.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using eTicaret.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicaret.Infrastructure.Context;
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<User> Users { get; set; } = default!;
+}
